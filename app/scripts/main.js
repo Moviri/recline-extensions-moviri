8e4ec11ccfcfcd5c82a3b4f7dd4892197ffc39ef
GioBaldac <giorgio.baldaccini@moviri.com>
2013-12-05 10:53:50 +0100

    MODA-292

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -28,6 +28,7 @@ require.config({
 
     'nvd3partial': '../bower_components/nvd3-gianlucaguarini/nv.d3',
     'nv.tooltips': '../bower_components/nvd3-gianlucaguarini/src/tooltip',
+    'file-saver': '../bower_components/file-saver/FileSaver'
 },
 shim: {
     'app' : { deps: [ 'jquery' ]},
